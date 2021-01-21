39a14fa2f4643bfefe8b90a43a790007699e199d
Radu Andries <radu.andries@sysdig.com>
2021-01-21 13:07:04 +0100

    Fix imports formatting
    Signed-off-by: Radu Andries <radu.andries@sysdig.com>

--- installer/util/aws.go
+++ installer/util/aws.go
@@ -4,11 +4,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"text/template"
+	"time"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"text/template"
-	"time"
 )
 
 const bucketTemplate = "kilt-{{ .AwsAccountID }}-{{ .AwsRegionName }}"

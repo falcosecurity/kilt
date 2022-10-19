ec929072ba997963527f2c2718a13642e9a38708
Francesco Racciatti <97034431+francesco-racciatti@users.noreply.github.com>
2022-10-19 16:56:05 +0200

    test: add tests covering Ref for tag values
    Signed-off-by: Francesco Racciatti <francesco.racciatti@sysdig.com>

--- runtimes/cloudformation/cfnpatcher/cfn_test.go
+++ runtimes/cloudformation/cfnpatcher/cfn_test.go
@@ -6,10 +6,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 
+	"github.com/Jeffail/gabs/v2"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
 	diff "github.com/yudai/gojsondiff"
 	"github.com/yudai/gojsondiff/formatter"
 )
@@ -20,18 +23,26 @@ var optInTests = [...]string{
 	"respect_ignores/opt_in_single_container",
 }
 
+var optPanicTests = [...]string{
+	"respect_ignores/panic_opt_exclude",
+	"respect_ignores/panic_opt_exclude_container",
+	"respect_ignores/panic_opt_include",
+	"respect_ignores/panic_opt_include_container",
+}
+
 var defaultTests = [...]string{
 	"respect_ignores/opt_out_default",
 	"respect_ignores/opt_out_ignored",
 	"respect_ignores/opt_out_ignore_single_container",
 
-	"patching/entrypoint",
 	"patching/command",
+	"patching/entrypoint",
 	"patching/ref",
 	"patching/ref_command",
 	"patching/ref_env",
-	"patching/volumes_from",
+	"patching/ref_tags",
 	"patching/tags",
+	"patching/volumes_from",
 }
 
 const defaultConfig = `
@@ -151,6 +162,160 @@ func TestPatchingForLogGroup(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
 			runTest(t, test.Name, l.WithContext(context.Background()), test.Config)
+		})
+	}
+}
+
+func TestOptTagPanic(t *testing.T) {
+	l := log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Caller().Logger()
+
+	for _, testName := range optPanicTests {
+		t.Run(testName, func(t *testing.T) {
+			defer func() { recover() }()
+
+			runTest(t, testName, l.WithContext(context.Background()),
+				Configuration{
+					Kilt:         defaultConfig,
+					OptIn:        true,
+					RecipeConfig: "{}",
+					UseRepositoryHints: false,
+				})
+		})
+	}
+}
+
+func TestIsFuncOptKey(t *testing.T) {
+ 	tests := []struct {
+		key string
+		out bool
+	}{
+		{
+			"kilt-ignore",
+			true,
+		},
+		{
+			"kilt-include",
+			true,
+		},
+		{
+			"kilt-ignore-containers",
+			true,
+		},
+		{
+			"kilt-include-containers",
+			true,
+		},
+		{
+			"so-long-and-thanks-for-all-the-fish",
+			false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.key, func(t *testing.T) {
+			assert.Equal(t, test.out, isOptTagKey(test.key), "OptIn/Out key not recognized")
+		})
+	}
+}
+
+func TestGetOptTags(t *testing.T) {
+ 	tests := []struct {
+		name     string
+		json     string
+		expected map[string]string
+	}{
+		{
+			name: `no-properties`,
+			json: `{
+"McGuffin": {
+	"Tags":[
+		{
+			"Key": "SoLong",
+			"Value": "AndThanksForAllTheFish"
+		}
+	]}
+}`,
+			expected: make(map[string]string),
+		},
+		{
+			name: `no-tags`,
+			json: `{
+"Properties": {
+	"Accio":[
+		{
+			"Key": "SoLong",
+			"Value": "AndThanksForAllTheFish"
+		}
+	]}
+}`,
+			expected: make(map[string]string),
+		},
+		{
+			name: `no-opt-tags`,
+			json: `{
+"Properties": {
+	"Tags":[
+		{
+			"Key": "SoLong",
+			"Value": "AndThanksForAllTheFish"
+		},
+		{
+			"Key": "TimeIsAnIllusion",
+			"Value": "LunchtimeDoublySo"
+		}
+	]}
+}`,
+			expected: make(map[string]string),
+		},
+		{
+			name: `all-opt-tags`,
+			json: `{
+"Properties": {
+	"Tags": [
+		{
+			"Key": "SoLong",
+			"Value": "AndThanksForAllTheFish"
+		},
+		{
+			"Key": "TimeIsAnIllusion",
+			"Value": "LunchtimeDoublySo"
+		},
+		{
+			"Key": "kilt-ignore",
+			"Value": "nanananananaBatman"
+		},
+		{
+			"Key": "kilt-include",
+			"Value": "gimmeGimmeGimmeFriedChicken"
+		},
+		{
+			"Key": "kilt-ignore-containers",
+			"Value": "expelliarmus"
+		},
+		{
+			"Key": "kilt-include-containers",
+			"Value": "accioContainer"
+		}
+	]}
+}`,
+			expected: map[string]string {
+				"kilt-ignore": "nanananananaBatman",
+				"kilt-include": "gimmeGimmeGimmeFriedChicken",
+				"kilt-ignore-containers": "expelliarmus",
+				"kilt-include-containers": "accioContainer",
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			jsonParsed, err := gabs.ParseJSON([]byte(tc.json))
+			if err != nil {
+				panic(err)
+			}
+			mm := getOptTags(jsonParsed)
+			eq := reflect.DeepEqual(tc.expected, mm)
+			if !eq {
+				assert.Fail(t, "maps do not match")
+			}
 		})
 	}
 }
